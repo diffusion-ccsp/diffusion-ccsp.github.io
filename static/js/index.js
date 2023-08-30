6ffddcbe812f13bdbb7be0b541924cb904d6b43f
diffusion-ccsp <136114844+diffusion-ccsp@users.noreply.github.com>
2023-08-29 21:50:55 -0400

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -33,19 +33,46 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
-// 
-// function updateResultVideo() {
-//   var task = document.getElementById("single-menu-replay").value;
-//
-//   console.log("qpred", "media/results/" + task + ".mp4");
-//
-//   var video = document.getElementById("result-video");
-//   video.src = "media/results/" + task + ".mp4";
-//   video.playbackRate = 0.5;
-//   video.play();
-//
-//   var table1 = document.getElementById("result-table");
-//   table1.setAttribute("w3-include-html", "result-"+task+".html");
-//
-//   includeHTML()
-// }
+
+function updateResultVideo() {
+  var combination = document.getElementById("single-menu-replay").value;
+
+  var table1 = document.getElementById("result-table");
+  table1.setAttribute("w3-include-html", "result_"+combination+".html");
+
+  includeHTML()
+
+  var video = document.getElementById("result-video");
+  video.playbackRate = 0.5;
+  video.play();
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    // console.log(elmnt.getAttribute("w3-include-html"))
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      console.log('includeHTML.file', file)
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
